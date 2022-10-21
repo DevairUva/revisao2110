dd42e4e8be9ebb5904bb8632b82669f91c169464
Devair Martins <devair.martins@gmail.com>
2022-10-21 20:56:53 -0300

    trocando o fundo

--- js/script.js
+++ js/script.js
@@ -9,5 +9,27 @@ myBody.append(myDiv);
 myBody.prepend(myDiv);
 
 function print(){
-    alert("vem que vem!")
+    alert("vem que vem!");
 }
+
+const myInput = document.querySelector('input');
+myInput.onkeydown = function() {
+    console.log('digitei');
+}
+
+const myH2 = document.querySelector('h2');
+myH2.addEventListener('mouseover', naruto); //onmouseover sem o 'on'
+
+
+function naruto(){
+    alert ('o evento');
+}
+
+const myColor = document.querySelector('h3');
+myColor.style.backgroundColor = 'green';
+
+// onclick
+// ondblclick
+// onmouseover
+// onkeyup
+
